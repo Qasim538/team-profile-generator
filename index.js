ea4f924c97697f23595cdb0b640651bb18480824
Qasim <mq12113@gmail.com>
2023-06-02 22:27:40 +0100

    finalise the code

--- index.js
+++ index.js
@@ -1,17 +1,13 @@
-const Manager = require("./lib/Manager");
+// node modules
+const inquirer = require("inquirer");
+const fs = require("fs");
+const generateTeam = require("./src/page-template.js");
+
+// lib modules
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
-const inquirer = require("inquirer");
-const path = require("path");
-const fs = require("fs");
+const Manager = require("./lib/Manager");
 
-const OUTPUT_DIR = path.resolve(__dirname, "output");
-const outputPath = path.join(OUTPUT_DIR, "team.html");
-
-const render = require("./src/page-template.js");
-
-
-// TODO: Write Code to gather information about the development team members, and render the HTML file.
 // Array for answers to questions
 const newStaffMemberData = [];
 
@@ -133,5 +129,4 @@ function createTeam () {
     "utf-8"
   );
 }
-
 
