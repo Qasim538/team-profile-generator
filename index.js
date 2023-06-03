c0b9e9755aeb912b8d95cde7fa87f9ae3775cfa3
Qasim <mq12113@gmail.com>
2023-06-03 11:49:51 +0100

    fixed few bugs

--- index.js
+++ index.js
@@ -2,8 +2,6 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
 const generateTeam = require("./src/page-template.js");
-
-// lib modules
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
 const Manager = require("./lib/Manager");
